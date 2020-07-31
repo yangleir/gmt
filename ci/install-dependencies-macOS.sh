b9b8c0931b4502cffbc72899bfa20e5b93f33a82
Dongdong Tian <seisman.info@gmail.com>
2020-07-31 02:04:27 -0400

    Run on macOS

--- ci/install-dependencies-macOS.sh
+++ ci/install-dependencies-macOS.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Bash script to install GMT dependencies on Ubuntu.
+# Bash script to install GMT dependencies on macOS via Homebrew
 #
 # Environmental variables that can control the installation:
 #
@@ -14,18 +14,17 @@ BUILD_DOCS="${BUILD_DOCS:-false}"
 RUN_TESTS="${RUN_TESTS:-false}"
 
 # packages for compiling GMT
-packages="build-essential cmake ninja-build libcurl4-gnutls-dev libnetcdf-dev \
-          libgdal-dev libfftw3-dev libpcre3-dev liblapack-dev libglib2.0-dev \
-          ghostscript curl"
+packages="cmake ninja curl netcdf gdal fftw pcre2 ghostscript"
+
 # packages for build documentations
 if [ "$BUILD_DOCS" = "true" ]; then
-    packages+=" python3-pip python3-setuptools python3-wheel graphicsmagick ffmpeg"
+    packages+=" graphicsmagick ffmpeg sphinx"
 fi
 # packages for runing GMT tests
 if [ "$RUN_TESTS" = "true" ]; then
-    packages+=" graphicsmagick gdal-bin"
+    packages+=" graphicsmagick"
 fi
 
 # Install packages
-sudo apt-get update
-sudo apt-get install -y --no-install-recommends --no-install-suggests $packages
+brew update
+berw install ${packages}
